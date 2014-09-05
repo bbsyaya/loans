204b471473a86018e463d2fa8ac57ba38e0d3a20
will <545512533@qq.com>
2014-09-05 15:36:33 +0800

    webview

---
+++ src/com/will/loans/ui/activity/WebBrowser.java
@@ -0,0 +1,61 @@
+
+package com.will.loans.ui.activity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.will.loans.R;
+
+public class WebBrowser extends BaseActivity {
+    private WebView mWeb;
+
+    public static final String URL_STRING = "url_string";
+
+    private String mUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        mUrl = getIntent().getExtras().getString(URL_STRING);
+        setContentView(R.layout.web_browser);
+        mWeb = (WebView) findViewById(R.id.web_view);
+        mWeb.setWebChromeClient(new MyWebChrome());
+        mWeb.setWebViewClient(new MyWebViewClient());
+        mWeb.loadUrl(mUrl);
+    }
+
+    class MyWebChrome extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            // TODO Auto-generated method stub
+            super.onProgressChanged(view, newProgress);
+        }
+
+    }
+
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onLoadResource(view, url);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+        }
+
+    }
+
+}
