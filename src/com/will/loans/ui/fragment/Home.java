fc343871060beac663961e975581b18b36a9f5a4
lxyzhh11 <270680516@qq.com>
2014-09-16 11:33:49 +0800

    //update iwant

--- src/com/will/loans/ui/fragment/Home.java
+++ src/com/will/loans/ui/fragment/Home.java
@@ -1,5 +1,7 @@
-
 package com.will.loans.ui.fragment;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import android.os.Bundle;
 import android.os.Handler;
@@ -18,204 +20,242 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.androidquery.AQuery;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 import com.will.loans.R;
 import com.will.loans.beans.bean.BannerItem;
 import com.will.loans.constant.ServerInfo;
 import com.will.loans.utils.ScreenProperties;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Home extends BaseFragment implements OnClickListener {
-    private ViewPager viewPager;
 
-    private RadioGroup groupPoint;
+	private PullToRefreshScrollView homePRSV;
 
-    private List<BannerItem> wheel;
+	private ViewPager viewPager;
 
-    private int currentPage;
+	private RadioGroup groupPoint;
 
-    private AQuery aq;
+	private List<BannerItem> wheel;
 
-    private final int MESSAGE_WHAT = 0;
+	private int currentPage;
 
-    private final long FLING_PAGE_INTERVAL = 3000;
+	private AQuery aq;
 
-    private Handler handler = new Handler() {
+	private final int MESSAGE_WHAT = 0;
 
-        @Override
-        public void handleMessage(Message msg) {
-            if (currentPage == wheel.size() + 2) {
-                currentPage = 2;
-            }
-            viewPager.setCurrentItem(currentPage);
-        }
-    };
+	private final long FLING_PAGE_INTERVAL = 3000;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        return inflater.inflate(R.layout.fragment_home, null);
-    }
+	private Handler handler = new Handler() {
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        aq = new AQuery(getActivity(), view);
-        setTitleText(view, R.string.tab_mine, R.string.refresh, R.string.tab_home);
-        setTitleVisible(view, View.VISIBLE, View.VISIBLE, View.VISIBLE);
-        ((Button) view.findViewById(R.id.title_btn_right)).setOnClickListener(this);
-        ((Button) view.findViewById(R.id.title_btn_left)).setOnClickListener(this);
+		@Override
+		public void handleMessage(Message msg) {
+			if (currentPage == wheel.size() + 2) {
+				currentPage = 2;
+			}
+			viewPager.setCurrentItem(currentPage);
+		}
+	};
 
-        groupPoint = (RadioGroup) view.findViewById(R.id.rg_points);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_home, null);
+	}
 
-        viewPager = (ViewPager) view.findViewById(R.id.vp_ads);
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		aq = new AQuery(getActivity(), view);
+		setTitleText(view, R.string.tab_mine, R.string.refresh,
+				R.string.tab_home);
+		setTitleVisible(view, View.VISIBLE, View.VISIBLE, View.VISIBLE);
+		((Button) view.findViewById(R.id.title_btn_right))
+				.setOnClickListener(this);
+		((Button) view.findViewById(R.id.title_btn_left))
+				.setOnClickListener(this);
 
-        initViewPager();
-    }
+		groupPoint = (RadioGroup) view.findViewById(R.id.rg_points);
 
-    private void initViewPager() {
-        viewPager = (ViewPager) getView().findViewById(R.id.vp_ads);
+		homePRSV = (PullToRefreshScrollView) view.findViewById(R.id.homePRSV);
+		viewPager = (ViewPager) view.findViewById(R.id.vp_ads);
 
-        initViewPagerBound();
+		initRefreshView();
+		initViewPager();
+	}
 
-        if (wheel == null) {
-            return;
-        }
+	private void getData() {
 
-        LinkedList<View> pageViews = new LinkedList<View>();
-        addPageViews(pageViews);
-        addPointView(pageViews.size() - 2);
+	}
 
-        viewPager.setAdapter(new WheelPagerAdapter(getActivity(), pageViews, wheel));
-        viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+	/**
+	 * 初始化下拉刷新監聽器
+	 */
+	private void initRefreshView() {
+		homePRSV.setOnRefreshListener(new OnRefreshListener<ScrollView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ScrollView> refreshView) {
+				getData();
+			}
+		});
+	}
 
-            @Override
-            public void onPageSelected(int arg0) {
-                //                renderBrief(arg0);
-                adjustWheel(arg0);
-                adjustPoint(arg0);
-                sendMessageForNextPage(arg0);
-            }
+	private void initViewPager() {
+		viewPager = (ViewPager) getView().findViewById(R.id.vp_ads);
 
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
+		initViewPagerBound();
 
-            }
+		if (wheel == null) {
+			return;
+		}
 
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
-            }
-        });
+		LinkedList<View> pageViews = new LinkedList<View>();
+		addPageViews(pageViews);
+		addPointView(pageViews.size() - 2);
 
-        viewPager.setCurrentItem(1);
+		viewPager.setAdapter(new WheelPagerAdapter(getActivity(), pageViews,
+				wheel));
+		viewPager.setOnPageChangeListener(new OnPageChangeListener() {
 
-        currentPage = 2;
-        handler.sendEmptyMessageDelayed(MESSAGE_WHAT, FLING_PAGE_INTERVAL);
+			@Override
+			public void onPageSelected(int arg0) {
+				// renderBrief(arg0);
+				adjustWheel(arg0);
+				adjustPoint(arg0);
+				sendMessageForNextPage(arg0);
+			}
 
-    }
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
 
-    private void initViewPagerBound() {
-        // 先算出图片长在屏幕中占多少英寸
-        float xInch = (ScreenProperties.getScreenWidth() / ScreenProperties.getXdpi());
-        //      Log.e("xInch", xInch + "Inch");
-        // 根据图片宽长比例算出高应该占多少英寸
-        double yInch = xInch * (15 / 32.0);
-        //      Log.e("yInch", yInch + "Inch");
-        // 再根据Y轴方向上每英寸多少像素算出图片高应该有多少像素
-        int viewPagerHeight = (int) (ScreenProperties.getXdpi() * yInch);
-        //      Log.e("viewPagerWidth", ScreenProperties.getScreenWidth() + "");
-        //      Log.e("viewPagerHeight", viewPagerHeight + "");
-        RelativeLayout viewPagerBound = (RelativeLayout) getView()
-                .findViewById(R.id.rlyt_viewpager);
-        viewPagerBound.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
-                viewPagerHeight));
-    }
+			}
 
-    private void addPageViews(LinkedList<View> pageViews) {
-        for (BannerItem videoItem : wheel) {
-            ImageView imageView = new ImageView(getActivity());
-            imageView.setScaleType(ScaleType.CENTER_CROP);
-            aq.id(imageView).image(ServerInfo.IMAGE_PREFIX + videoItem.url, true, true);
-            pageViews.add(imageView);
-        }
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
 
-        for (int i = 0; i < wheel.size(); i++) {
-            if (i == 0) {
-                ImageView imageView = extraImageView(i);
-                pageViews.addLast(imageView);
-            }
+		viewPager.setCurrentItem(1);
 
-            if (i == wheel.size() - 1) {
-                ImageView imageView = extraImageView(i);
-                pageViews.addFirst(imageView);
-            }
-        }
-    }
+		currentPage = 2;
+		handler.sendEmptyMessageDelayed(MESSAGE_WHAT, FLING_PAGE_INTERVAL);
 
-    private ImageView extraImageView(int i) {
-        ImageView imageView = new ImageView(getActivity());
-        imageView.setScaleType(ScaleType.CENTER_CROP);
-        aq.id(imageView).image(ServerInfo.IMAGE_PREFIX + wheel.get(i).url, true, true);
+	}
 
-        return imageView;
+	private void initViewPagerBound() {
+		// 先算出图片长在屏幕中占多少英寸
+		float xInch = (ScreenProperties.getScreenWidth() / ScreenProperties
+				.getXdpi());
+		// Log.e("xInch", xInch + "Inch");
+		// 根据图片宽长比例算出高应该占多少英寸
+		double yInch = xInch * (15 / 32.0);
+		// Log.e("yInch", yInch + "Inch");
+		// 再根据Y轴方向上每英寸多少像素算出图片高应该有多少像素
+		int viewPagerHeight = (int) (ScreenProperties.getXdpi() * yInch);
+		// Log.e("viewPagerWidth", ScreenProperties.getScreenWidth() + "");
+		// Log.e("viewPagerHeight", viewPagerHeight + "");
+		RelativeLayout viewPagerBound = (RelativeLayout) getView()
+				.findViewById(R.id.rlyt_viewpager);
+		viewPagerBound.setLayoutParams(new LinearLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, viewPagerHeight));
+	}
 
-    }
+	private void addPageViews(LinkedList<View> pageViews) {
+		for (BannerItem videoItem : wheel) {
+			ImageView imageView = new ImageView(getActivity());
+			imageView.setScaleType(ScaleType.CENTER_CROP);
+			aq.id(imageView).image(ServerInfo.IMAGE_PREFIX + videoItem.url,
+					true, true);
+			pageViews.add(imageView);
+		}
 
-    private void addPointView(int count) {
-        if (count > 0) {
-            for (int i = 0; i < count; i++) {
-                RadioButton radBtn = new RadioButton(getActivity());
-                radBtn.setClickable(false);
-                int width = (int) ScreenProperties.getScreenDensity() * 25;
-                RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(width,
-                        LayoutParams.WRAP_CONTENT);
-                radBtn.setButtonDrawable(R.drawable.page_control_sel);
-                groupPoint.addView(radBtn, params);
-            }
-        }
+		for (int i = 0; i < wheel.size(); i++) {
+			if (i == 0) {
+				ImageView imageView = extraImageView(i);
+				pageViews.addLast(imageView);
+			}
 
-        View v = groupPoint.getChildAt(0);
-        RadioButton radioBtn = (RadioButton) v;
-        radioBtn.setChecked(true);
-    }
+			if (i == wheel.size() - 1) {
+				ImageView imageView = extraImageView(i);
+				pageViews.addFirst(imageView);
+			}
+		}
+	}
 
-    private void adjustWheel(int currentPage) {
-        if (currentPage == wheel.size() + 1) {
-            viewPager.setCurrentItem(1, false);
-        }
+	private ImageView extraImageView(int i) {
+		ImageView imageView = new ImageView(getActivity());
+		imageView.setScaleType(ScaleType.CENTER_CROP);
+		aq.id(imageView).image(ServerInfo.IMAGE_PREFIX + wheel.get(i).url,
+				true, true);
 
-        if (currentPage == 0) {
-            viewPager.setCurrentItem(wheel.size(), false);
-        }
-    }
+		return imageView;
 
-    private void adjustPoint(int pageIndex) {
-        if (pageIndex == 0 || pageIndex == wheel.size() + 1) {
-            return;
-        }
+	}
 
-        View child = groupPoint.getChildAt(pageIndex - 1);
-        if (child instanceof RadioButton) {
-            RadioButton radBtn = (RadioButton) child;
-            radBtn.setChecked(true);
-        }
-    }
+	private void addPointView(int count) {
+		if (count > 0) {
+			for (int i = 0; i < count; i++) {
+				RadioButton radBtn = new RadioButton(getActivity());
+				radBtn.setClickable(false);
+				int width = (int) ScreenProperties.getScreenDensity() * 25;
+				RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(
+						width, LayoutParams.WRAP_CONTENT);
+				radBtn.setButtonDrawable(R.drawable.page_control_sel);
+				groupPoint.addView(radBtn, params);
+			}
+		}
 
-    private void sendMessageForNextPage(int nowPage) {
-        if (handler.hasMessages(MESSAGE_WHAT)) {
-            handler.removeMessages(MESSAGE_WHAT);
-        }
+		View v = groupPoint.getChildAt(0);
+		RadioButton radioBtn = (RadioButton) v;
+		radioBtn.setChecked(true);
+	}
 
-        currentPage = nowPage + 1;
-        handler.sendEmptyMessageDelayed(MESSAGE_WHAT, FLING_PAGE_INTERVAL);
-    }
+	private void adjustWheel(int currentPage) {
+		if (currentPage == wheel.size() + 1) {
+			viewPager.setCurrentItem(1, false);
+		}
 
-    @Override
-    public void onClick(View v) {
-        // TODO Auto-generated method stub
+		if (currentPage == 0) {
+			viewPager.setCurrentItem(wheel.size(), false);
+		}
+	}
 
-    }
+	private void adjustPoint(int pageIndex) {
+		if (pageIndex == 0 || pageIndex == wheel.size() + 1) {
+			return;
+		}
+
+		View child = groupPoint.getChildAt(pageIndex - 1);
+		if (child instanceof RadioButton) {
+			RadioButton radBtn = (RadioButton) child;
+			radBtn.setChecked(true);
+		}
+	}
+
+	private void sendMessageForNextPage(int nowPage) {
+		if (handler.hasMessages(MESSAGE_WHAT)) {
+			handler.removeMessages(MESSAGE_WHAT);
+		}
+
+		currentPage = nowPage + 1;
+		handler.sendEmptyMessageDelayed(MESSAGE_WHAT, FLING_PAGE_INTERVAL);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.title_btn_right:
+
+			break;
+		case R.id.title_btn_left:
+
+			break;
+		default:
+			break;
+		}
+	}
 }
--- src/com/will/loans/ui/fragment/IWant.java
+++ src/com/will/loans/ui/fragment/IWant.java
@@ -1,27 +1,161 @@
-
 package com.will.loans.ui.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 import com.will.loans.R;
 
-public class IWant extends BaseFragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        return inflater.inflate(R.layout.fragment_mine, null);
-    }
-    
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-    	super.onViewCreated(view, savedInstanceState);
-    	((TextView) view.findViewById(R.id.title_tv)).setText(R.string.tab_mine);
-        ((Button) view.findViewById(R.id.title_btn_left)).setText(R.string.login);
-        ((Button) view.findViewById(R.id.title_btn_right)).setText(R.string.refresh);
-    }
+public class IWant extends BaseFragment implements OnClickListener {
+
+	private PullToRefreshScrollView homePRSV;
+
+	private LinearLayout todayEarnBtn;
+	private LinearLayout amountEarnBtn;
+	private LinearLayout amountMoneyBtn;
+	private LinearLayout hasMoneyBtn;
+	private LinearLayout remainMoneyBtn;
+	private LinearLayout tradeHistoryBtn;
+	private LinearLayout amountPointBtn;
+
+	/**
+	 * 今日收益日期（09月12日收益（元））
+	 */
+	private TextView todayDateTV;
+	/**
+	 * 今日收益（元）
+	 */
+	private TextView todayEarnTV;
+	/**
+	 * 累计收益
+	 */
+	private TextView amountEarnTV;
+	/**
+	 * 总资产
+	 */
+	private TextView amountMoneyTV;
+	/**
+	 * 持有资产
+	 */
+	private TextView hasMoneyTV;
+	/**
+	 * 账户余额
+	 */
+	private TextView remainMoneyTV;
+	/**
+	 * 总积分
+	 */
+	private TextView amountPointTV;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_mine, null);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		((TextView) view.findViewById(R.id.title_tv))
+				.setText(R.string.tab_mine);
+		((Button) view.findViewById(R.id.title_btn_left))
+				.setText(R.string.login);
+		((Button) view.findViewById(R.id.title_btn_right))
+				.setText(R.string.refresh);
+
+		initButton(view);
+		initTextView(view);
+
+		// homePRSV = (PullToRefreshScrollView)
+		// view.findViewById(R.id.minePRSV);
+		// initRefreshView();
+
+		getDate();
+	}
+
+	/**
+	 * 请求接口数据
+	 */
+	private void getDate() {
+		// TODO 网络请求
+	}
+
+	private void updateView() {
+		// TODO 请求结束后调用刷新视图
+		todayDateTV.setText("");
+		todayEarnTV.setText("");
+		amountEarnTV.setText("");
+		amountMoneyTV.setText("");
+		hasMoneyTV.setText("");
+		remainMoneyTV.setText("");
+		amountPointTV.setText("");
+	}
+
+	/**
+	 * 初始化下拉刷新監聽器
+	 */
+	private void initRefreshView() {
+		homePRSV.setOnRefreshListener(new OnRefreshListener<ScrollView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ScrollView> refreshView) {
+				getDate();
+			}
+		});
+	}
+
+	private void initButton(View view) {
+		todayEarnBtn = (LinearLayout) view.findViewById(R.id.todayEarnBtn);
+		amountEarnBtn = (LinearLayout) view.findViewById(R.id.amountEarnBtn);
+		amountMoneyBtn = (LinearLayout) view.findViewById(R.id.amountMoneyBtn);
+		hasMoneyBtn = (LinearLayout) view.findViewById(R.id.hasMoneyBtn);
+		remainMoneyBtn = (LinearLayout) view.findViewById(R.id.remainMoneyBtn);
+		tradeHistoryBtn = (LinearLayout) view
+				.findViewById(R.id.tradeHistoryBtn);
+		amountPointBtn = (LinearLayout) view.findViewById(R.id.amountPointBtn);
+
+		todayEarnBtn.setOnClickListener(this);
+	}
+
+	private void initTextView(View view) {
+		todayDateTV = (TextView) view.findViewById(R.id.todayDateTV);
+		todayEarnTV = (TextView) view.findViewById(R.id.todayEarnTV);
+		amountEarnTV = (TextView) view.findViewById(R.id.amountEarnTV);
+		amountMoneyTV = (TextView) view.findViewById(R.id.amountMoneyTV);
+		hasMoneyTV = (TextView) view.findViewById(R.id.hasMoneyTV);
+		remainMoneyTV = (TextView) view.findViewById(R.id.remainMoneyTV);
+		amountPointTV = (TextView) view.findViewById(R.id.amountPointTV);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.todayEarnBtn:
+			break;
+		case R.id.amountEarnBtn:
+			break;
+		case R.id.amountMoneyBtn:
+			break;
+		case R.id.hasMoneyBtn:
+			break;
+		case R.id.remainMoneyBtn:
+			break;
+		case R.id.tradeHistoryBtn:
+			break;
+		case R.id.amountPointBtn:
+			break;
+		default:
+			break;
+		}
+	}
+
 }
