82016cf05f4b17bd908a71a1e507e5cbd0030c2b
will <545512533@qq.com>
2014-09-25 17:44:58 +0800

    //导航条beans类

--- src/com/will/loans/beans/bean/BannerItem.java
+++ src/com/will/loans/beans/bean/BannerItem.java
@@ -2,22 +2,34 @@
 package com.will.loans.beans.bean;
 
 public class BannerItem extends BaseBeans {
-	public String value;//: "http://app.longyinglicai.com/activity/jmh/jm.html",
+    public String value;//: "http://app.longyinglicai.com/activity/jmh/jm.html",
 
-	public String type;//: "ACTIVITY",
+    public String type;//: "ACTIVITY",
 
-	public String url;//: "http://app.longyinglicai.com/activity/jmh/images/yyjm_banner_ios.png",
+    public String url;//: "http://app.longyinglicai.com/activity/jmh/images/yyjm_banner_ios.png",
 
-	public int orderNum;//: 0
+    public int orderNum;//: 0
 
-	public BannerItem(String value, String type, String url, int orderNum) {
-		super();
-		this.value = value;
-		this.type = type;
-		this.url = url;
-		this.orderNum = orderNum;
-	}
+    public int id;//":1,
 
+    public String bannerName;//":"banner图片名称",
 
+    public String bannerDesc;//":"banner图片描述",
+
+    public Long createTime;//":1411315200000,
+
+    public String bannerUrl;//"":"img/banner.jpg",
+
+    public int status;//":0,
+
+    public String contUrl;//":"test/banner.html"
+
+    public BannerItem(String value, String type, String url, int orderNum) {
+        super();
+        this.value = value;
+        this.type = type;
+        this.url = url;
+        this.orderNum = orderNum;
+    }
 
 }
