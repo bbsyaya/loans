7accb077b1fdf958b1d024f0bf2ae68d85a387aa
lxyzhh11 <270680516@qq.com>
2014-09-26 14:42:04 +0800

    //fix login&verify

--- src/com/will/loans/ui/activity/FillPassword.java
+++ src/com/will/loans/ui/activity/FillPassword.java
@@ -1,12 +1,4 @@
-
 package com.will.loans.ui.activity;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.content.Intent;
 import android.graphics.Paint;
@@ -25,6 +17,13 @@ import com.will.loans.R;
 import com.will.loans.constant.ServerInfo;
 import com.will.loans.utils.SharePreferenceUtil;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FillPassword extends BaseTextActivity {
 	private EditText mPsw;
 
@@ -32,9 +31,9 @@ public class FillPassword extends BaseTextActivity {
 
 	private AQuery mAQuery;
 
-	private String mNum,token;
-	public static final String  NUM = "com.will.loans.num";
-	public static final String  TOKEN = "com.will.loans.token";
+	private String mNum, token;
+	public static final String NUM = "com.will.loans.num";
+	public static final String TOKEN = "com.will.loans.token";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -55,56 +54,70 @@ public class FillPassword extends BaseTextActivity {
 		mLogin.setOnClickListener(this);
 		findViewById(R.id.forget_psw).setOnClickListener(this);
 		mPsw.addTextChangedListener(this);
-		//设置下划线
-		((TextView) findViewById(R.id.forget_psw)).getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+		// 设置下划线
+		((TextView) findViewById(R.id.forget_psw)).getPaint().setFlags(
+				Paint.UNDERLINE_TEXT_FLAG);
 	}
 
 	public void buildParams() {
+		time = System.currentTimeMillis();
 		JSONObject jo = new JSONObject();
 		try {
 			jo.put("timeStamp", new Date().getTime());
 			jo.put("phoneNum", mNum);
 			jo.put("loginPsw", mPsw.getText().toString());
+			jo.put("sign", getMD5Code(time));
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("jsonData", jo.toString());
-		mAQuery.ajax(ServerInfo.LOGINBYPSW, params,
-				JSONObject.class, new AjaxCallback<JSONObject>() {
-			@Override
-			public void callback(String url, JSONObject object, AjaxStatus status) {
-				System.out.println(" " + object.toString());
-				if (object!=null) {
-					try {
-						String result = object.getString("resultflag");
-						SharePreferenceUtil.getUserPref(FillPassword.this).setToken(object.getString("token"));
-						SharePreferenceUtil.getUserPref(FillPassword.this).setUserId(object.getString("userid"));
-						SharePreferenceUtil.getUserPref(FillPassword.this).setUsername(mNum);
-						if (result.equals("0")) {
-							Toast.makeText(getApplication(), "登陆成功", 1*1000).show();
-							FillPassword.this.finish();
-						}else{
-							Toast.makeText(getApplication(), object.getString("resultMsg"), 1*1000).show();
+		mAQuery.ajax(ServerInfo.LOGINBYPSW, params, JSONObject.class,
+				new AjaxCallback<JSONObject>() {
+					@Override
+					public void callback(String url, JSONObject object,
+							AjaxStatus status) {
+						System.out.println(" " + object.toString());
+						if (object != null) {
+							try {
+								String result = object.optString("resultflag");
+								SharePreferenceUtil.getUserPref(
+										FillPassword.this).setToken(
+										object.optString("token"));
+								SharePreferenceUtil.getUserPref(
+										FillPassword.this).setUserId(
+										object.optString("userid"));
+								SharePreferenceUtil.getUserPref(
+										FillPassword.this).setUsername(mNum);
+								if (result.equals("0")) {
+									Toast.makeText(getApplication(), "登陆成功",
+											1 * 1000).show();
+									FillPassword.this.finish();
+								} else {
+									Toast.makeText(getApplication(),
+											object.optString("resultMsg"),
+											1 * 1000).show();
+								}
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 						}
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
 					}
-				}
-			}
-		});
+				});
 	}
 
 	private void initTop() {
 		findViewById(R.id.title_back).setVisibility(View.VISIBLE);
 		findViewById(R.id.title_back).setOnClickListener(this);
-		((TextView)findViewById(R.id.title_tv)).setText(R.string.fill_password);
+		((TextView) findViewById(R.id.title_tv))
+				.setText(R.string.fill_password);
 
 	}
 
 	@Override
-	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
 
 	}
 
@@ -128,7 +141,7 @@ public class FillPassword extends BaseTextActivity {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.forget_psw:
-			startActivity(new Intent(FillPassword.this,ForgetPassword.class));
+			startActivity(new Intent(FillPassword.this, ForgetPassword.class));
 			break;
 		case R.id.btn_login:
 			buildParams();
--- src/com/will/loans/ui/activity/FillVerifyCode.java
+++ src/com/will/loans/ui/activity/FillVerifyCode.java
@@ -1,4 +1,3 @@
-
 package com.will.loans.ui.activity;
 
 import android.content.Intent;
@@ -28,184 +27,190 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FillVerifyCode extends BaseTextActivity {
-    private EditText mVerifyCode;
+	private EditText mVerifyCode;
 
-    private Button mCountDown, mLogin;
+	private Button mCountDown, mLogin;
 
-    private String mNum, token;
+	private String mNum, token;
 
-    public static final String NUM = "com.will.loans.num";
+	public static final String NUM = "com.will.loans.num";
 
-    public static final String TOKEN = "com.will.loans.token";
+	public static final String TOKEN = "com.will.loans.token";
 
-    private AQuery mAQuery;
+	private AQuery mAQuery;
 
-    private CountDown mCountDownTimer;
+	private CountDown mCountDownTimer;
 
-    private Long millisInFuture = 60 * 1000L, countDownInterval = 1000L;
+	private final Long millisInFuture = 60 * 1000L, countDownInterval = 1000L;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        super.onCreate(savedInstanceState);
-        mNum = getIntent().getExtras().getString(NUM);
-        token = getIntent().getExtras().getString(TOKEN);
-        setContentView(R.layout.activity_fill_vrify_code);
-        init();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		mNum = getIntent().getExtras().getString(NUM);
+		token = getIntent().getExtras().getString(TOKEN);
+		setContentView(R.layout.activity_fill_vrify_code);
+		init();
+	}
 
-    public void buildParams() {
-        time = System.currentTimeMillis();
-        JSONObject jo = new JSONObject();
-        try {
-            jo.put("timeStamp", time);
-            jo.put("phoneNum", mNum);
-            jo.put("verCode", mVerifyCode.getText().toString());
-            jo.put("token", token);
-            jo.put("sign", getMD5Code(time));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("jsonData", jo.toString());
-        mAQuery.ajax(ServerInfo.REGISTERORLOGINBYMSG, params, JSONObject.class, mRegisterCallback);
-    }
+	public void buildParams() {
+		time = System.currentTimeMillis();
+		JSONObject jo = new JSONObject();
+		try {
+			jo.put("timeStamp", time);
+			jo.put("phoneNum", mNum);
+			jo.put("verCode", mVerifyCode.getText().toString());
+			jo.put("token", token);
+			jo.put("sign", getMD5Code(time));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("jsonData", jo.toString());
+		mAQuery.ajax(ServerInfo.REGISTERORLOGINBYMSG, params, JSONObject.class,
+				mRegisterCallback);
+	}
 
-    AjaxCallback<JSONObject> mRegisterCallback = new AjaxCallback<JSONObject>() {
-        @Override
-        public void callback(String url, JSONObject object, AjaxStatus status) {
-            super.callback(url, object, status);
-            Log.d("", object.toString());
-            if (status.getCode() == 200) {
-                //				mCustomProgressDialog.dismiss();
-            }
-            try {
-                String state = object.getString("resultflag");
-                if (state.equals("0")) {
-                    SharePreferenceUtil.getUserPref(FillVerifyCode.this).setToken(
-                            object.getString("token"));
-                    SharePreferenceUtil.getUserPref(FillVerifyCode.this).setUserId(
-                            object.getString("userid"));
-                    SharePreferenceUtil.getUserPref(FillVerifyCode.this).setUsername(mNum);
-                    startActivity(new Intent(FillVerifyCode.this, SetPassword.class).putExtra(
-                            SetPassword.SETTYPE, 0));
-                    FillVerifyCode.this.finish();
-                } else {
-                    Toast.makeText(getApplication(), object.getString("resultMsg"), 1 * 1000)
-                            .show();
-                }
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    };
+	AjaxCallback<JSONObject> mRegisterCallback = new AjaxCallback<JSONObject>() {
+		@Override
+		public void callback(String url, JSONObject object, AjaxStatus status) {
+			super.callback(url, object, status);
+			Log.d("", object.toString());
+			if (status.getCode() == 200) {
+				// mCustomProgressDialog.dismiss();
+			}
+			try {
+				String state = object.optString("resultflag");
+				if (state.equals("0")) {
+					SharePreferenceUtil.getUserPref(FillVerifyCode.this)
+							.setToken(object.optString("token"));
+					SharePreferenceUtil.getUserPref(FillVerifyCode.this)
+							.setUserId(object.optString("userid"));
+					SharePreferenceUtil.getUserPref(FillVerifyCode.this)
+							.setUsername(mNum);
+					startActivity(new Intent(FillVerifyCode.this,
+							SetPassword.class).putExtra(SetPassword.SETTYPE, 0));
+					FillVerifyCode.this.finish();
+				} else {
+					Toast.makeText(getApplication(),
+							object.optString("resultMsg"), 1 * 1000).show();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	};
 
-    private void init() {
-        initTop();
-        mCountDownTimer = new CountDown(millisInFuture, countDownInterval);
-        mAQuery = new AQuery(this);
-        findViewById(R.id.title_back).setVisibility(View.VISIBLE);
-        findViewById(R.id.title_back).setOnClickListener(this);
-        ((TextView) findViewById(R.id.title_tv)).setText(R.string.fill_verify_num);
-        mVerifyCode = (EditText) findViewById(R.id.et_veri_code);
-        mCountDown = (Button) findViewById(R.id.btn_count_down);
-        ((TextView) findViewById(R.id.tv_send_msg)).setText("已向" + mNum + "发送短信，请再输入框中填写验证码完成注册");
-        ;
+	private void init() {
+		initTop();
+		mCountDownTimer = new CountDown(millisInFuture, countDownInterval);
+		mAQuery = new AQuery(this);
+		findViewById(R.id.title_back).setVisibility(View.VISIBLE);
+		findViewById(R.id.title_back).setOnClickListener(this);
+		((TextView) findViewById(R.id.title_tv))
+				.setText(R.string.fill_verify_num);
+		mVerifyCode = (EditText) findViewById(R.id.et_veri_code);
+		mCountDown = (Button) findViewById(R.id.btn_count_down);
+		((TextView) findViewById(R.id.tv_send_msg)).setText("已向" + mNum
+				+ "发送短信，请再输入框中填写验证码完成注册");
+		;
 
-        mLogin = (Button) findViewById(R.id.btn_confirm);
-        mLogin.setOnClickListener(this);
-        mCountDown.setOnClickListener(this);
-        mVerifyCode.addTextChangedListener(this);
+		mLogin = (Button) findViewById(R.id.btn_confirm);
+		mLogin.setOnClickListener(this);
+		mCountDown.setOnClickListener(this);
+		mVerifyCode.addTextChangedListener(this);
 
-    }
+	}
 
-    private void initTop() {
-        // TODO Auto-generated method stub
+	private void initTop() {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    public void reGetVerycode() {
-        JSONObject jo = new JSONObject();
-        try {
-            jo.put("timeStamp", System.currentTimeMillis());
-            jo.put("phoneNum", mNum);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("jsonData", jo.toString());
-        mAQuery.ajax(ServerInfo.LOGINFIRST, params, LoginFirst.class,
-                new AjaxCallback<LoginFirst>() {
-                    @Override
-                    public void callback(String url, LoginFirst object, AjaxStatus status) {
-                        if (status != null && status.getCode() == 200) {
-                            Toaster.showShort(FillVerifyCode.this, "获取验证码成功！");
-                        }
-                    }
-                });
-    }
+	public void reGetVerycode() {
+		JSONObject jo = new JSONObject();
+		try {
+			jo.put("timeStamp", System.currentTimeMillis());
+			jo.put("phoneNum", mNum);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("jsonData", jo.toString());
+		mAQuery.ajax(ServerInfo.LOGINFIRST, params, LoginFirst.class,
+				new AjaxCallback<LoginFirst>() {
+					@Override
+					public void callback(String url, LoginFirst object,
+							AjaxStatus status) {
+						if (status != null && status.getCode() == 200) {
+							Toaster.showShort(FillVerifyCode.this, "获取验证码成功！");
+						}
+					}
+				});
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_confirm:
-                buildParams();
-                break;
-            case R.id.btn_count_down:
-                reGetVerycode();
-                mCountDownTimer.start();
-                mCountDown.setEnabled(false);
-                break;
-            case R.id.title_back:
-                finish();
-                break;
-            default:
-                break;
-        }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_confirm:
+			buildParams();
+			break;
+		case R.id.btn_count_down:
+			reGetVerycode();
+			mCountDownTimer.start();
+			mCountDown.setEnabled(false);
+			break;
+		case R.id.title_back:
+			finish();
+			break;
+		default:
+			break;
+		}
 
-    }
+	}
 
-    class CountDown extends CountDownTimer {
+	class CountDown extends CountDownTimer {
 
-        public CountDown(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
+		public CountDown(long millisInFuture, long countDownInterval) {
+			super(millisInFuture, countDownInterval);
+		}
 
-        @Override
-        public void onTick(long millisUntilFinished) {
-            mCountDown.setText(millisUntilFinished / 1000
-                    + getResources().getString(R.string.countdown_code));
+		@Override
+		public void onTick(long millisUntilFinished) {
+			mCountDown.setText(millisUntilFinished / 1000
+					+ getResources().getString(R.string.countdown_code));
 
-        }
+		}
 
-        @Override
-        public void onFinish() {
-            mCountDown.setEnabled(true);
-            mCountDown.setText(R.string.reget_verifycode);
-        }
+		@Override
+		public void onFinish() {
+			mCountDown.setEnabled(true);
+			mCountDown.setText(R.string.reget_verifycode);
+		}
 
-    }
+	}
 
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        // TODO Auto-generated method stub
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        if (s.length() > 0) {
-            mLogin.setEnabled(true);
-        } else {
-            mLogin.setEnabled(false);
-        }
-    }
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		if (s.length() > 0) {
+			mLogin.setEnabled(true);
+		} else {
+			mLogin.setEnabled(false);
+		}
+	}
 
-    @Override
-    public void afterTextChanged(Editable s) {
-        // TODO Auto-generated method stub
+	@Override
+	public void afterTextChanged(Editable s) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
 }
