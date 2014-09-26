ecd6949315c8b32f9caeb0c610cde2ef2af27bfa
will <545512533@qq.com>
2014-09-26 16:52:24 +0800

    //设置交易密码

--- src/com/will/loans/pay/BasePayActivity.java
+++ src/com/will/loans/pay/BasePayActivity.java
@@ -1,9 +1,5 @@
-package com.will.loans.pay;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+package com.will.loans.pay;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -21,160 +17,169 @@ import android.widget.Button;
 
 import com.will.loans.R;
 
-public abstract class BasePayActivity extends Activity implements Callback,
-Runnable {
-	public static final String LOG_TAG = "PayDemo";
-	private Context mContext = null;
-	protected Handler mHandler = null;
-	protected ProgressDialog mLoadingDialog = null;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
-	public static final int PLUGIN_VALID = 0;
-	public static final int PLUGIN_NOT_INSTALLED = -1;
-	public static final int PLUGIN_NEED_UPGRADE = 2;
+public abstract class BasePayActivity extends Activity implements Callback, Runnable {
+    public static final String LOG_TAG = "PayDemo";
 
-	/*****************************************************************
-	 * mMode参数解释： "00" - 启动银联正式环境 "01" - 连接银联测试环境
-	 *****************************************************************/
-	private final String mMode = "01";
-	private static final String TN_URL_01 = "http://202.101.25.178:8080/sim/gettn";
+    private Context mContext = null;
 
-	protected Button nextBtn;
+    protected Handler mHandler = null;
 
-	private final View.OnClickListener mClickListener = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			mLoadingDialog = ProgressDialog.show(mContext, // context
-					"", // title
-					"正在努力的获取tn中,请稍候...", // message
-					true); // 进度是否是不确定的，这只和创建进度条有关
+    protected ProgressDialog mLoadingDialog = null;
 
-			/*************************************************
-			 * 步骤1：从网络开始,获取交易流水号即TN
-			 ************************************************/
-			new Thread(BasePayActivity.this).start();
-		}
-	};
+    public static final int PLUGIN_VALID = 0;
 
-	public abstract void doStartUnionPayPlugin(Activity activity, String tn,
-			String mode);
+    public static final int PLUGIN_NOT_INSTALLED = -1;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mContext = this;
-		mHandler = new Handler(this);
+    public static final int PLUGIN_NEED_UPGRADE = 2;
 
-		setContentView(R.layout.activity_pay);
+    /*****************************************************************
+     * mMode参数解释： "00" - 启动银联正式环境 "01" - 连接银联测试环境
+     *****************************************************************/
+    private final String mMode = "01";
 
-		nextBtn = (Button) findViewById(R.id.nextBtn);
+    private static final String TN_URL_01 = "http://202.101.25.178:8080/sim/gettn";
 
-		afterSetContentView();
+    protected Button nextBtn;
 
-	}
+    private final View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mLoadingDialog = ProgressDialog.show(mContext, // context
+                    "", // title
+                    "正在努力的获取tn中,请稍候...", // message
+                    true); // 进度是否是不确定的，这只和创建进度条有关
 
-	protected void afterSetContentView() {
+            /*************************************************
+             * 步骤1：从网络开始,获取交易流水号即TN
+             ************************************************/
+            new Thread(BasePayActivity.this).start();
+        }
+    };
 
-	}
+    public abstract void doStartUnionPayPlugin(Activity activity, String tn, String mode);
 
-	@Override
-	public boolean handleMessage(Message msg) {
-		Log.e(LOG_TAG, " " + "" + msg.obj);
-		if (mLoadingDialog.isShowing()) {
-			mLoadingDialog.dismiss();
-		}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        mHandler = new Handler(this);
 
-		String tn = "";
-		if (msg.obj == null || ((String) msg.obj).length() == 0) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setTitle("错误提示");
-			builder.setMessage("网络连接失败,请重试!");
-			builder.setNegativeButton("确定",
-					new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.dismiss();
-				}
-			});
-			builder.create().show();
-		} else {
+        setContentView(R.layout.activity_pay);
 
-			tn = (String) msg.obj;
-			/*************************************************
-			 * 步骤2：通过银联工具类启动支付插件
-			 ************************************************/
-			doStartUnionPayPlugin(this, tn, mMode);
-		}
+        nextBtn = (Button) findViewById(R.id.nextBtn);
 
-		return false;
-	}
+        afterSetContentView();
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		/*************************************************
-		 * 步骤3：处理银联手机支付控件返回的支付结果
-		 ************************************************/
-		if (data == null) {
-			return;
-		}
+    }
 
-		String msg = "";
-		/*
-		 * 支付控件返回字符串:success、fail、cancel 分别代表支付成功，支付失败，支付取消
-		 */
-		String str = data.getExtras().getString("pay_result");
-		if (str.equalsIgnoreCase("success")) {
-			msg = "支付成功！";
-		} else if (str.equalsIgnoreCase("fail")) {
-			msg = "支付失败！";
-		} else if (str.equalsIgnoreCase("cancel")) {
-			msg = "用户取消了支付";
-		}
+    protected void afterSetContentView() {
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("支付结果通知");
-		builder.setMessage(msg);
-		builder.setInverseBackgroundForced(true);
-		// builder.setCustomTitle();
-		builder.setNegativeButton("确定", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-			}
-		});
-		builder.create().show();
-	}
+    }
 
-	@Override
-	public void run() {
-		String tn = null;
-		InputStream is;
-		try {
+    @Override
+    public boolean handleMessage(Message msg) {
+        Log.e(LOG_TAG, " " + "" + msg.obj);
+        if (mLoadingDialog.isShowing()) {
+            mLoadingDialog.dismiss();
+        }
 
-			String url = TN_URL_01;
+        String tn = "";
+        if (msg.obj == null || ((String) msg.obj).length() == 0) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("错误提示");
+            builder.setMessage("网络连接失败,请重试!");
+            builder.setNegativeButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.create().show();
+        } else {
 
-			URL myURL = new URL(url);
-			URLConnection ucon = myURL.openConnection();
-			ucon.setConnectTimeout(120000);
-			is = ucon.getInputStream();
-			int i = -1;
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			while ((i = is.read()) != -1) {
-				baos.write(i);
-			}
+            tn = (String) msg.obj;
+            /*************************************************
+             * 步骤2：通过银联工具类启动支付插件
+             ************************************************/
+            doStartUnionPayPlugin(this, tn, mMode);
+        }
 
-			tn = baos.toString();
-			is.close();
-			baos.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        return false;
+    }
 
-		Message msg = mHandler.obtainMessage();
-		msg.obj = tn;
-		mHandler.sendMessage(msg);
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /*************************************************
+         * 步骤3：处理银联手机支付控件返回的支付结果
+         ************************************************/
+        if (data == null) {
+            return;
+        }
 
-	int startpay(Activity act, String tn, int serverIdentifier) {
-		return 0;
-	}
+        String msg = "";
+        /*
+         * 支付控件返回字符串:success、fail、cancel 分别代表支付成功，支付失败，支付取消
+         */
+        String str = data.getExtras().getString("pay_result");
+        if (str.equalsIgnoreCase("success")) {
+            msg = "支付成功！";
+        } else if (str.equalsIgnoreCase("fail")) {
+            msg = "支付失败！";
+        } else if (str.equalsIgnoreCase("cancel")) {
+            msg = "用户取消了支付";
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("支付结果通知");
+        builder.setMessage(msg);
+        builder.setInverseBackgroundForced(true);
+        // builder.setCustomTitle();
+        builder.setNegativeButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                BasePayActivity.this.finish();
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    public void run() {
+        String tn = null;
+        InputStream is;
+        try {
+
+            String url = TN_URL_01;
+
+            URL myURL = new URL(url);
+            URLConnection ucon = myURL.openConnection();
+            ucon.setConnectTimeout(120000);
+            is = ucon.getInputStream();
+            int i = -1;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            while ((i = is.read()) != -1) {
+                baos.write(i);
+            }
+
+            tn = baos.toString();
+            is.close();
+            baos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Message msg = mHandler.obtainMessage();
+        msg.obj = tn;
+        mHandler.sendMessage(msg);
+    }
+
+    int startpay(Activity act, String tn, int serverIdentifier) {
+        return 0;
+    }
 }
